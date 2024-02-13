f063c4b062ec5b877bf26cf789c144f40aa10534
rephlexions <ovidiu92@gmail.com>
2024-02-13 19:36:02 +0100

    adding dist

--- dist/assets/chevron-left-Qs2BVtbp.js
+++ dist/assets/chevron-left-Qs2BVtbp.js
@@ -1,4 +1,4 @@
-import{O as e}from"./index-wRVETa5M.js";/**
+import{O as e}from"./index-GWcXG4Hy.js";/**
  * @license lucide-vue-next v0.323.0 - ISC
  *
  * This source code is licensed under the ISC license.
