4530b60c80f754ff0e3e82fd95f437b0885750df
rephlexions <ovidiu92@gmail.com>
2024-02-13 19:31:44 +0100

    adding dist

--- dist/assets/chevron-left-fZx3apGI.js
+++ dist/assets/chevron-left-fZx3apGI.js
@@ -1,4 +1,4 @@
-import{O as e}from"./index-GoKqbDie.js";/**
+import{O as e}from"./index-wRVETa5M.js";/**
  * @license lucide-vue-next v0.323.0 - ISC
  *
  * This source code is licensed under the ISC license.
