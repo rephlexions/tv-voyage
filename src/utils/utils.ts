9abe0a7d9c3252e0eec024860b6ebea8f9984db6
Ovidiu Cara <ovidiu92@gmail.com>
2024-06-18 11:31:15 +0200

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,107 +1,107 @@
-import type { Episode, Genre, Season, Show } from '@/types/types'
+// import type { Genre } from '@/types/types'
 
-function normalizeShows(data: any[]): Show[] {
-  return data.map((obj) => {
-    return {
-      id: obj.id,
-      name: obj.name,
-      image: {
-        medium: obj.image.medium,
-        original: obj.image.original
-      },
-      rating: {
-        average: obj.rating.average
-      },
-      genres: obj.genres,
-      premiered: obj.premiered,
-      status: obj.status,
-      summary: obj.summary,
-      language: obj.language,
-      officialSite: obj.network ? obj.network?.officialSite : null
-    }
-  })
-}
-function normalizeEpisodes(data: any[]): Episode[] {
-  return data.map((obj) => {
-    return {
-      id: obj.id,
-      name: obj.name,
-      number: obj.number,
-      season: obj.season,
-      airDate: obj.airdate,
-      rating: {
-        average: obj.rating.average
-      }
-    }
-  })
-}
-function normalizeSearchResults(data: any[]): Show[] {
-  const results: Show[] = []
-  data[0].forEach((el: { show: any }) => {
-    results.push(el.show)
-  })
+// function normalizeShows(data: any[]): Show[] {
+//   return data.map((obj) => {
+//     return {
+//       id: obj.id,
+//       name: obj.name,
+//       image: {
+//         medium: obj.image.medium,
+//         original: obj.image.original
+//       },
+//       rating: {
+//         average: obj.rating.average
+//       },
+//       genres: obj.genres,
+//       premiered: obj.premiered,
+//       status: obj.status,
+//       summary: obj.summary,
+//       language: obj.language,
+//       officialSite: obj.network ? obj.network?.officialSite : null
+//     }
+//   })
+// }
+// function normalizeEpisodes(data: any[]): Episode[] {
+//   return data.map((obj) => {
+//     return {
+//       id: obj.id,
+//       name: obj.name,
+//       number: obj.number,
+//       season: obj.season,
+//       airDate: obj.airdate,
+//       rating: {
+//         average: obj.rating.average
+//       }
+//     }
+//   })
+// }
+// function normalizeSearchResults(data: any[]): Show[] {
+//   const results: Show[] = []
+//   data[0].forEach((el: { show: any }) => {
+//     results.push(el.show)
+//   })
 
-  return results.map((obj: Show) => {
-    return {
-      id: obj.id,
-      name: obj.name,
-      image: obj.image
-        ? {
-            medium: obj.image.medium,
-            original: obj.image.original
-          }
-        : null
-    }
-  })
-}
+//   return results.map((obj: Show) => {
+//     return {
+//       id: obj.id,
+//       name: obj.name,
+//       image: obj.image
+//         ? {
+//             medium: obj.image.medium,
+//             original: obj.image.original
+//           }
+//         : null
+//     }
+//   })
+// }
 
-function filterShows(data: Show[], type: Genre): Show[] {
-  const { name } = type
-  if (name === 'all') return data
-  data = data.filter((show) => show.genres!.includes(name))
-  return data
-}
+// function filterShows(data: Show[], type: Genre): Show[] {
+//   const { name } = type
+//   if (name === 'all') return data
+//   data = data.filter((show) => show.genres!.includes(name))
+//   return data
+// }
 
-function normalizeGenres(data: any[]): Genre[] {
-  const genres = data.map((show) => show.genres).flat()
-  const uniqueGenres = [...new Set(genres)]
-  return uniqueGenres.map((genre, index) => {
-    return { id: index, name: genre }
-  })
-}
+// function normalizeGenres(data: any[]): Genre[] {
+//   const genres = data.map((show) => show.genres).flat()
+//   const uniqueGenres = [...new Set(genres)]
+//   return uniqueGenres.map((genre, index) => {
+//     return { id: index, name: genre }
+//   })
+// }
 
-function getBackgroundImage(data: any[]): string {
-  const index = data.findIndex(
-    (image) => image.type === 'background' && image.resolutions.original.width === 1920
-  )
-  return data[index].resolutions.original.url
-}
+// function getBackgroundImage(data: any[]): string {
+//   const index = data.findIndex(
+//     (image) => image.type === 'background' && image.resolutions.original.width === 1920
+//   )
+//   return data[index].resolutions.original.url
+// }
 
-function shuffle(array: any[]): any[] {
-  const arrayCopy = [...array]
-  for (let i = arrayCopy.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]]
-  }
-  return arrayCopy
-}
+// function shuffle(array: any[]): any[] {
+//   const arrayCopy = [...array]
+//   for (let i = arrayCopy.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1))
+//     ;[arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]]
+//   }
+//   return arrayCopy
+// }
 
-function groupEpisodesBySeason(episodes: Episode[]): Season {
-  return episodes.reduce((grouped: Season, episode: Episode) => {
-    const key = String(episode.season)
-    grouped[key] = grouped[key] || []
-    grouped[key].push(episode)
-    return grouped
-  }, {})
-}
+// function groupEpisodesBySeason(episodes: Episode[]): Season {
+//   return episodes.reduce((grouped: Season, episode: Episode) => {
+//     const key = String(episode.season)
+//     grouped[key] = grouped[key] || []
+//     grouped[key].push(episode)
+//     return grouped
+//   }, {})
+// }
 
-export {
-  normalizeShows,
-  normalizeEpisodes,
-  filterShows,
-  normalizeGenres,
-  getBackgroundImage,
-  shuffle,
-  groupEpisodesBySeason,
-  normalizeSearchResults
-}
+// export {
+//   normalizeShows,
+//   normalizeEpisodes,
+//   filterShows,
+//   normalizeGenres,
+//   getBackgroundImage,
+//   shuffle,
+//   groupEpisodesBySeason,
+//   normalizeSearchResults
+// }
