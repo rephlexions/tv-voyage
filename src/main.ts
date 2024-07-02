c2d46686d8f899c08ad9fe4d0629348b8262e2fe
Ovidiu Cara <ovidiu92@gmail.com>
2024-07-02 11:03:55 +0200

    improved store fetch calls on mount

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import App from './App.vue';
 import router from './router';
 import { useGenresStore } from '@/store/genres';
 import { useMediaStore } from '@/store/media';
+import { useToast } from '@/components/ui/toast/use-toast';
 
 const pinia = createPinia();
 const app = createApp(App);
+const { toast } = useToast();
 
 app.use(router);
 app.use(pinia);
@@ -16,12 +18,27 @@ app.mount('#app');
 
 const genresStore = useGenresStore();
 const mediaStore = useMediaStore();
-Promise.allSettled([
-  genresStore.fetchMovieGenres(),
-  genresStore.fetchTvGenres(),
-  mediaStore.fetchNowPlaying(),
-  mediaStore.fetchTopRated(),
-  mediaStore.fetchUpcoming(),
-  mediaStore.fetchTrendingTv(),
-  mediaStore.fetchTrendingMovies()
-]);
+
+try {
+  const results = await Promise.allSettled([
+    genresStore.fetchMovieGenres(),
+    genresStore.fetchTvGenres(),
+    mediaStore.fetchNowPlaying(),
+    mediaStore.fetchTopRated(),
+    mediaStore.fetchUpcoming(),
+    mediaStore.fetchTrendingTv(),
+    mediaStore.fetchTrendingMovies()
+  ]);
+
+  results.forEach((result) => {
+    if (result.status === 'rejected') {
+      throw new Error(`${result.reason}`);
+    }
+  });
+} catch (error) {
+  toast({
+    title: 'An error occurred',
+    description: `${error}`,
+    variant: 'destructive'
+  });
+}
