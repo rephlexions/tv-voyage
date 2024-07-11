5a36efb368522bacb5cf048742b5efc9ea4644ab
Ovidiu Cara <ovidiu92@gmail.com>
2024-07-11 20:02:56 +0200

    vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import TopNav from './components/TopNav.vue';
 import Toaster from '@/components/ui/toast/Toaster.vue';
-import { Analytics } from '@vercel/analytics/next';
 </script>
 
 <template>
   <TopNav></TopNav>
   <Toaster />
-  <Analytics />
   <RouterView></RouterView>
 </template>
 
