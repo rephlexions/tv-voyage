fcc782e37c8a78fb7b1fe9f7af467ab6d97c6da9
Ovidiu Cara <ovidiu92@gmail.com>
2024-07-12 09:59:36 +0200

    vercel analytics inject

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from './router';
 import { useGenresStore } from '@/store/genres';
 import { useMediaStore } from '@/store/media';
 import { useToast } from '@/components/ui/toast/use-toast';
+import { inject } from '@vercel/analytics';
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -15,6 +16,8 @@ app.use(router);
 app.use(pinia);
 
 app.mount('#app');
+
+inject();
 
 const genresStore = useGenresStore();
 const mediaStore = useMediaStore();
