f4eff44c716c25768e6faabbe50bae3b20f52838
Ovidiu Cara <ovidiu92@gmail.com>
2025-02-16 14:36:12 +0100

    added vite plugin devtools

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()]
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    target: 'esnext'
-  }
+	plugins: [vue(), vueDevTools()],
+	css: {
+		postcss: {
+			plugins: [tailwind(), autoprefixer()],
+		},
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	build: {
+		target: 'esnext',
+	},
 });
