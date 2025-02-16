2d9da68df07cf1394e2fccef54759873ef07db47
Ovidiu Cara <ovidiu92@gmail.com>
2025-02-16 14:36:28 +0100

    refactor TopNav to use RouterLink for homepage navigation

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { RouterLink, useRouter } from 'vue-router';
 
 const search = ref('');
 const router = useRouter();
@@ -13,7 +13,7 @@ const setSearch = () => {
 </script>
 <template>
 	<nav class="bg-primary flex justify-between align-middle h-16 pr-10 pl-10 pt-4">
-		<a href="/" class="hover:cursor-pointer" aria-label="Homepage" role="link"> <img src="../assets/logo.svg" alt="Home" /></a>
+		<RouterLink to="/" class="hover:cursor-pointer"> <img src="../assets/logo.svg" alt="Home" /></RouterLink>
 
 		<div class="flex gap-4">
 			<Input v-model="search" class="dark" id="search" type="text" placeholder="Search" aria-label="Search input" />
